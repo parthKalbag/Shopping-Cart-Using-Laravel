00e93ae6fdd0eff5d58234c74fea325a4e95986c
Parth Kalbag <10bparth@gmail.com>
2020-08-08 14:09:50 +0530

    Added redirecting in ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,19 +28,17 @@ class ProductController extends Controller
     public function update($product) {
         $product = Product::findOrFail($product);
         $product->update(request()->all());
-
-        return $product;
+        return redirect()->route('products.index');
     }
 
     public function destroy($product) {
         $product = Product::findOrFail($product);
         $product->delete();
-
-        return $product;
+        return redirect()->route('products.index');
     }
 
     public function store() {
         $product = Product::create(request()->all());
-        return $product;
+        return redirect()->route('products.index');
     }
 }
