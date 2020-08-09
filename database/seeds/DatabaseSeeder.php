c57486782e1f352c89d865b898973900e99fbd83
Parth Kalbag <10bparth@gmail.com>
2020-08-09 18:45:15 +0530

    Added simple relationships

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use App\Product;
+use App\User;
+use App\Order;
+use App\Payment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $users = factory(User::class, 20)->create();
+        $orders = factory(Order::class, 10)->make()->each(function ($order) use ($users){
+            $order->customer_id = $users->random()->id;
+            $order->save();
+            $payment = factory(Payment::class)->make();
+            $order->payment()->save($payment);
+        });
         $products = factory(Product::class, 50)->create();
     }
 }
